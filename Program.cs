d2f5e8fef1a7bbcfe542e852c0e1c9b03dca0d38
Anh Nguyen <anguyen@swin.edu.au>
2021-04-22 14:28:11 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodsExample_Grp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int age = CalcAge(1324);
+            System.Console.WriteLine(age);
+
+        }
+
+        
+        public static int CalcAge(int YearOfBirth) {
+            int age = GetCurrentYear() - YearOfBirth;
+            return age;
+        }
+
+        public static int GetCurrentYear() {
+            return DateTime.Now.Year;
+        }
+    }
+}
